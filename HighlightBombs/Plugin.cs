d0056dccb14e69248af887a68480062d1d32ea9f
Meivyn <793322+Meivyn@users.noreply.github.com>
2022-06-06 16:50:46 -0400

    Use PluginMetadata to get assembly instead of reflection

--- HighlightBombs/Plugin.cs
+++ HighlightBombs/Plugin.cs
@@ -1,7 +1,7 @@
 ﻿using IPA;
 using System.Collections;
-using System.Reflection;
 using HarmonyLib;
+using IPA.Loader;
 using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 
@@ -10,27 +10,31 @@ namespace HighlightBombs
     [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
+        private readonly PluginMetadata _metadata;
         private readonly Harmony _harmony;
 
+        internal static Plugin Instance { get; private set; } = null!;
         internal static IPALogger Log { get; private set; } = null!;
 
         [Init]
-        public Plugin(IPALogger logger)
+        public Plugin(IPALogger logger, PluginMetadata metadata)
         {
+            Instance = this;
             Log = logger;
+            _metadata = metadata;
             _harmony = new Harmony("com.meivyn.HighlightBombs");
         }
 
         [OnEnable]
         public void OnEnable()
         {
-            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            _harmony.PatchAll(_metadata.Assembly);
             SharedCoroutineStarter.instance.StartCoroutine(LoadQuickOutlineMaterials());
         }
 
         public IEnumerator LoadQuickOutlineMaterials()
         {
-            var quickOutlineBundleRequest = AssetBundle.LoadFromStreamAsync(Assembly.GetExecutingAssembly().GetManifestResourceStream("HighlightBombs.QuickOutline.Resources.outlineBundle"));
+            var quickOutlineBundleRequest = AssetBundle.LoadFromStreamAsync(_metadata.Assembly.GetManifestResourceStream("HighlightBombs.QuickOutline.Resources.outlineBundle"));
             yield return quickOutlineBundleRequest;
             var quickOutlineBundle = quickOutlineBundleRequest.assetBundle;
             if (quickOutlineBundle == null)
